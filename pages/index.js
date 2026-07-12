1577f83206e2595ed76451fcd555d3f02747b082
Lucky <137119819+LuckyBreak647@users.noreply.github.com>
2026-07-12 02:24:42 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
     return <h1>Como eu era tolo uma criança inocente, acreditava que podia ter um mundo diferente</h1>
-}
+} 
+<h2>DA MORTE EU VOLTEI, (VO VO VOLETEI) </h2>
 
 export default Home;
