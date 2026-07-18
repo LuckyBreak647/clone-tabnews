029233aa5d2fa9ea553a0a4bf49227642be4e270
Lucky <137119819+LuckyBreak647@users.noreply.github.com>
2026-07-18 04:45:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-function Home(){
-    return <h1>Como eu era tolo uma criança inocente, acreditava que podia ter um mundo diferente DA MORTE EU VOLTEI, (VO VO VOLTEI)</h1>
-} 
-<h2>DA MORTE EU VOLTEI, (VO VO VOLETEI) </h2>
-
+function Home() {
+  return (
+    <h1>
+      Como eu era tolo uma criança inocente, acreditava que podia ter um mundo
+      diferente DA MORTE EU VOLTEI, (VO VO VOLTEI)
+    </h1>
+  );
+}
+function Teste() {
+  console.log("teste");
+}
+function teste2() {
+  console.log("indentação errada");
+}
 
 export default Home;
